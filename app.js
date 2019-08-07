20fd89862d46c2d56e8128044b6fdcfac91ec39b
Erica P <erica.parra@rocketmail.com>
2019-08-07 13:31:06 -0700

    html , css, js added

---
+++ app.js
@@ -0,0 +1,40 @@
+window.addEventListener("load" , ()=> {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy ="https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/adfd8b5e3e7bf17e922d321651dc71f3/${lat},${long}`;
+
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                //console.log(data);
+                const {temperature, summary, icon } = data.currently;
+                //set Dom Elements from the api 
+                temperatureDegree.textContent = temperature + " F ";
+                temperatureDescription.textContent = "Conditions Today:  " + summary;
+                locationTimezone.textContent = data.timezone;
+                // set icon 
+                setIcons(icon, document.querySelector(".icon"));
+            });
+        });
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
